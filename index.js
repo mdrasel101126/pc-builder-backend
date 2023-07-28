ae180e42886b11917b422a0b07e3226e6f86c832
mdrasel101126 <mdrasel101126@gmail.com>
2023-07-29 00:00:25 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Pc Builder Server is Running!");
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const database = client.db("PcBuilder");
+    const productCollection = database.collection("products");
+    app.get("/products", async (req, res) => {
+      const products = await productCollection.find({}).toArray();
+      res.send(products);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const product = await productCollection.findOne({ _id: id });
+      res.send(product);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch((error) => {
+  console.log(error);
+});
+
+app.listen(port, () => {
+  console.log("Server is running in port ", port);
+});
